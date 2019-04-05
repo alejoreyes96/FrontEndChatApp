3b7f5055a9a79c329b056ea28413bf2e6f9a90e5
Alejandro <alejandro.reyes3@upr.edu>
2019-04-05 16:54:05 -0400

    Added routing to components. Added messages services

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { GroupChatComponent } from './group-chat/group-chat.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LogInComponent},
+
+  { path: 'dashboard', component: DashboardComponent},
+  { path: 'group-chat', component: GroupChatComponent},
+  { path: 'log-in', component: LogInComponent},
+  { path: 'register', component: RegisterComponent}
+]; 
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
