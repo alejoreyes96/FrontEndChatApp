7c88affaadb9c31d414e34511bdec26d450e1bf8
Kahlil <kahlil.fonseca@upr.edu>
2019-04-05 16:55:43 -0400

    Working with login

--- src/app/log-in/log-in.component.ts
+++ src/app/log-in/log-in.component.ts
@@ -1,14 +1,62 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-log-in',
   templateUrl: './log-in.component.html',
   styleUrls: ['./log-in.component.css']
 })
+
 export class LogInComponent implements OnInit {
+  validEmail: boolean;
+  state: any = [{
+    email: '',
+    password: ''
+  }]
+  constructor() {
+  
 
-  constructor() { }
+    this.validEmail = false;
 
+    this.handleChange = this.handleChange.bind(this);
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+}
+ setState(name: any){
+  this.state.email = name;
+ }
+
+handleChange(e){
+    let target = e.target;
+    let value = target.type === 'checkbox' ? target.checked : target.value;
+    let name = target.name;
+
+    this.setState({
+        [name]:value
+    });
+}
+
+handleSubmit(e){
+    e.preventDefault();
+    this.validEmail = this.validateEmail(this.state.email);
+    if(!this.validEmail){
+        console.log('Not Valid!');
+    }
+    console.log('The form was submitted with the following data:');
+    console.log(this.state);
+}
+
+validateEmail(inputText){
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if(inputText.match(mailformat)){
+        //document.form1.text1.focus();
+        return true;
+        } else {
+            alert("You have entered an invalid email address!");
+            //document.form1.text1.focus();
+            return false;
+        }
+    }
   ngOnInit() {
   }
 
