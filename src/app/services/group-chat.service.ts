987a7a2fba7b6a9953ed1bd154db375dffd10712
Alejandro <alejandro.reyes3@upr.edu>
2019-04-06 01:38:39 -0400

    Implemented http request: GET on dashboard and group-chat components using services. Added image handling and uploading per message attributes. Loads messages on Backend on entry/init.

--- src/app/services/group-chat.service.ts
+++ src/app/services/group-chat.service.ts
@@ -1,27 +1,34 @@
-import { Injectable } from '@angular/core';
+import { Injectable, } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { HttpHeaders } from '@angular/common/http';
 import { HttpResponse } from '@angular/common/http';
-import { Observable } from 'rxjs';
 
-export interface Config {
+export interface GroupChatsConfig {
   GroupChats: any[]
 
 }
+
 const httpOptions = {
   headers: new HttpHeaders({
     'Content-Type':  'application/json'
   })
 }; 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GroupChatService {
- 
-    chatUrl = 'http://127.0.0.1:5000/FFMA/users/1/groupChats/'
-    getChats(){
-      return this.http.get<Config>(this.chatUrl);
+  constructor(private http:HttpClient) { 
+  }
+
+    userId: number;
+    chatUrl: string;
+    getChats(data){
+      this.createChatUrl(data);
+      return this.http.get<GroupChatsConfig>(this.chatUrl);
+    }
+    createChatUrl(data){
+      this.chatUrl = 'http://127.0.0.1:5000/FFMA/users/'+data+'/groupChats/';
+
     }
     
-  constructor(private http:HttpClient) { }
 }
